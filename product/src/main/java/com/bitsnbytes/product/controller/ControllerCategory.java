a7f96558eb57d8f79dabd31f6004b1d3fa913789
sumitkprasad123 <sumitkprasad123@gmail.com>
2025-07-06 22:51:37 +0530

    completed basic curd of product and category

--- product/src/main/java/com/bitsnbytes/product/controller/ControllerCategory.java
+++ product/src/main/java/com/bitsnbytes/product/controller/ControllerCategory.java
@@ -2,26 +2,41 @@ package com.bitsnbytes.product.controller;
 
 import com.bitsnbytes.product.dto.CategoryDTO;
 import com.bitsnbytes.product.service.CategoryService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/api/categories")
+@AllArgsConstructor
 public class ControllerCategory {
-    @Autowired
     private CategoryService categoryService;
     //get all categories
+    @GetMapping
+    public List<CategoryDTO> getAllCategories(){
+        return categoryService.getAllCategories();
+    }
+
     //create Categories
     @PostMapping
     public ResponseEntity<CategoryDTO> createCategory(@RequestBody CategoryDTO categoryDTO){
         return new ResponseEntity<>(categoryService.createCategory(categoryDTO), HttpStatus.CREATED);
     }
-    //get category by id
-    //delete category
+
+    // get category by id
+    @GetMapping("/{id}")
+    public CategoryDTO getCategoryById(@PathVariable Long id){
+        return categoryService.getCategoryById(id);
+    }
+
+    // delete category
+    @DeleteMapping("/{id}")
+    public String deleteCategory(@PathVariable Long id){
+        return categoryService.deleteCategory(id);
+    }
 }
